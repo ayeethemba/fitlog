174846d4d73b5b5d76588a2947310b091decd6a6
Themba Chika <chikatx@dukes.jmu.edu>
2026-06-30 12:04:17 -0400

    feat: add auth routes - register and login with JWT

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+console.log('JWT_SECRET: ', process.env.JWT_SECRET)
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const db = require('./db');
+
 
 const app = express();
 
@@ -8,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
 // Test route
 app.get('/api/health', (req, res) => {
