dadb90a949f5a91057336e2a05e86879d6cd745b
Themba Chika <chikatx@dukes.jmu.edu>
2026-07-11 13:07:33 -0400

    feat: add stats to dashboard - total workouts, streak, most frequent exercise

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from '../utils/api.js';
 
 function Dashboard() {
     const navigate = useNavigate();
+    const [stats, setStats] = useState({});
+
+    useEffect(() => {
+        async function fetchStats() {
+            try {
+                const data = await api.get('/api/stats');
+                setStats(data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchStats();
+    }, [])
 
     function handleLogout() {
         localStorage.removeItem("token");
@@ -13,6 +28,11 @@ function Dashboard() {
             <h1>Dashboard</h1>
             <p>Welcome to Fitlog!</p>
             <button onClick={handleLogout}>Logout</button>
+            
+            <h2>Your stats: </h2>
+            <p>Total Workouts: {stats.total_workouts}</p>
+            <p>Current Streak: {stats.streak} Days</p>
+            <p>Most Frequent exercise: {stats.mostFrequent}</p>
         </div>
     )
 }
