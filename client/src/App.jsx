29ab0ab65786ed51689371cd7a99e576cd023ab6
Themba Chika <chikatx@dukes.jmu.edu>
2026-07-14 10:36:48 -0400

    Added route redirection for empty domain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import Signup from './pages/Signup.jsx';
 import PrivateRoute from "./components/PrivateRoute.jsx"
 import LogWorkout  from './pages/LogWorkout.jsx';
 import WorkoutHistory from './pages/WorkoutHistory.jsx';
+import { Navigate } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login"></Navigate>} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<PrivateRoute>
           <Dashboard />
